d0645b37c21fd19e7b399deb28a44b87eff1de40
kent1 <kent1@arscenic.info>
2014-05-27 17:58:07 +0200

    Revoir les chaînes de langue des publications

--- lang/hals_publication_fr.php
+++ lang/hals_publication_fr.php
@@ -5,7 +5,21 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
+	'icone_creer_hals_publication' => 'Créer une publication HAL',
+	'icone_hals_publication' => 'Publication HAL',
+	'icone_hals_publications' => 'Publications HAL',
 	'icone_modifier_publication' => 'Modifier cette publication',
+	'info_aucun_hals_publication' => 'Aucune publication HAL',
+	'info_1_hals_publication' => 'Une publication HAL',
+	'info_nb_hals_publications' => '@nb@ publications HAL',
+	'info_citation_complete' => 'Citation complète :',
+	'info_citation_reference' => 'Citation de référence :',
+	'info_editeur' => 'Éditeur',
+	'info_identifiant' => 'Identifiant :',
+	'info_page' => 'Pages :',
+	
+	// T
+	'titre_logo_hals_publication' => 'Logo de la publication',
 );
 
 ?>
