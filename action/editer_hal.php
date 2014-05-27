d212742e36be80a794cb4cfbc7fc5bbc88f770e1
kent1 <kent1@arscenic.info>
2014-05-27 14:05:08 +0200

    Edition des dépôts HAL

---
+++ action/editer_hal.php
@@ -0,0 +1,119 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function action_editer_hal_dist($arg=null) {
+
+	if (is_null($arg)){
+		$securiser_action = charger_fonction('securiser_action', 'inc');
+		$arg = $securiser_action();
+	}
+
+	if (!$id_hal = intval($arg)){
+		$id_hal = hal_inserer();
+	}
+
+	if (!$id_hal)
+		return array(0,'');
+
+	$err = hal_modifier($id_hal);
+
+	return array($id_hal,$err);
+}
+
+
+/**
+ * Inserer un nouveau dépot HAL en base
+ *
+ * @return bool
+ */
+function hal_inserer() {
+
+	$champs = array(
+		'statut' => 'prop',
+		'date' => date('Y-m-d H:i:s'));
+	
+	// Envoyer aux plugins
+	$champs = pipeline('pre_insertion',
+		array(
+			'args' => array(
+				'table' => 'spip_hals',
+			),
+			'data' => $champs
+		)
+	);
+
+	$id_hal = sql_insertq("spip_hals", $champs);
+	pipeline('post_insertion',
+		array(
+			'args' => array(
+				'table' => 'spip_hals',
+				'id_objet' => $id_hal
+			),
+			'data' => $champs
+		)
+	);
+
+	return $id_hal;
+}
+
+/**
+ * Modifier un dépot HAL
+ *
+ * $c est un contenu (par defaut on prend le contenu via _request())
+ *
+ * @param int $id_hal
+ * @param array|bool $set
+ * @return string
+ */
+function hal_modifier($id_hal, $set=false) {
+	include_spip('inc/modifier');
+	$c = collecter_requests(
+		// white list
+		array(
+		 'titre', 'descriptif', 'authid'
+		),
+		// black list
+		array('statut', 'date'),
+		// donnees eventuellement fournies
+		$set
+	);
+	$q = '';
+	$hal_api = "http://api-preprod.archives-ouvertes.fr/search/";
+	if(isset($c['authid']))
+		$q .= 'authId_i:'.$c['authid'];
+	
+	$hal_api = parametre_url($hal_api,'q',$q);
+	$hal_api = parametre_url(parametre_url($hal_api,'sort','modifiedDate_s desc'),'rows','200');
+
+	$c['url_syndic'] = $hal_api;
+	
+	// Si le dépot est publie, invalider les caches et demander sa reindexation
+	$t = sql_getfetsel("statut", "spip_hals", "id_hal=".intval($id_hal));
+	if ($t == 'publie') {
+		$invalideur = "id='hal/$id_hal'";
+		$indexation = true;
+	}
+
+	if ($err = objet_modifier_champs('hal', $id_hal,
+		array(
+			'nonvide' => array('titre' => _T('info_sans_titre')),
+			'invalideur' => $invalideur,
+			'indexation' => $indexation
+		),
+		$c))
+		return $err;
+
+	// Modification de statut, changement de rubrique ?
+	$c = collecter_requests(array('date', 'statut'),array(),$set);
+	include_spip('action/editer_objet');
+	$err = objet_instituer('hal',$id_hal, $c);
+
+	return $err;
+}
+
+function instituer_hal($id_hal, $c){
+	include_spip('action/editer_objet');
+	return objet_instituer('hal',$id_hal, $c);
+}
+?>
---
+++ formulaires/editer_hal.php
@@ -0,0 +1,57 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+/**
+ * Formulaire de création et d'édition d'un dépôt HAL
+ */
+
+include_spip('inc/actions');
+include_spip('inc/editer');
+
+/**
+ * Chargement des valeurs par défaut du formulaire
+ * 
+ * @param int|string $id_hal Identifiant numérique du point ou 'new' pour un nouveau
+ */
+function formulaires_editer_hal_charger_dist($id_hal='new', $retour='', $config_fonc='hals_edit_config', $row=array(), $hidden=''){
+	$valeurs = formulaires_editer_objet_charger('hal', $id_hal, '', '', $retour, '',$config_fonc,$row,$hidden);
+	return $valeurs;
+}
+
+/**
+ * Vérification des valeurs du formulaire
+ * 
+ * 2 champs sont obligatoires :
+ * -* Son titre
+ * -* Son authid
+ * 
+ * @param int|string $id_hal Identifiant numérique du point ou 'new' pour un nouveau
+ * 
+ */
+function formulaires_editer_hal_verifier_dist($id_hal='new', $retour='', $config_fonc='hals_edit_config', $row=array(), $hidden=''){
+	$erreurs = formulaires_editer_objet_verifier('hal', $id_hal,array('titre','authid'));
+	return $erreurs;
+}
+
+// Choix par defaut des options de presentation
+function hals_edit_config($row)
+{
+	global $spip_lang;
+
+	$config = $GLOBALS['meta'];
+	$config['lignes'] = 8;
+	$config['langue'] = $spip_lang;
+	return $config;
+}
+/**
+ * Traitement des valeurs du formulaire
+ * 
+ * @param int|string $id_hal Identifiant numérique du point ou 'new' pour un nouveau
+ * 
+ */
+function formulaires_editer_hal_traiter_dist($id_hal='new', $retour='', $config_fonc='hals_edit_config', $row=array(), $hidden=''){
+	return formulaires_editer_objet_traiter('hal', $id_hal, '', '', $retour, '',$config_fonc,$row,$hidden);
+}
+
+?>
