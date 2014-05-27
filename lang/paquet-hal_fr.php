1c8308560b040e2db75eea4aef0f731052367135
kent1 <kent1@arscenic.info>
2014-05-27 14:12:05 +0200

    Le fichier de langue du paquet.xml
    Renommage du titre

---
+++ lang/paquet-hal_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'hal_description' => 'Récupérer les documents mis en ligne sur HAL dans SPIP',
+	'hal_slogan' => 'Gestion des dépôts HAL dans SPIP'
+);
+
+?>
