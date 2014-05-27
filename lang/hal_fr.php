e3f592643f7be1c781d71d00465f0bd61acc7939
kent1 <kent1@arscenic.info>
2014-05-27 14:06:53 +0200

    Première micro intégration dans le privé et les chaînes de langue de base

---
+++ lang/hal_fr.php
@@ -0,0 +1,31 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'avis_echec_recuperation' => 'La récupération du dépôt a échoué : impossible d\'accéder à l\'API.',
+
+	// E
+	'explication_authid' => 'Identifiant numérique unique d\'un auteur sur HAL',
+
+	// I
+	'icone_hal_reference' => 'Dépôt HAL',
+	'icone_hals_references' => 'Dépôts HAL référencés',
+	'icone_modifier_hal' => 'Modifier le dépôt',
+	'icone_referencer_nouveau_hal' => 'Référencer un nouveau dépôt',
+	'info_1_hal' => '1 dépôt HAL',
+	'info_aucun_hal' => 'Aucun dépôt HAL',
+	'info_nb_hals' => '@nb@ dépôts HAL',
+	'info_statut_hal' => 'Le dépôt est :',
+	
+	// L
+	'label_authid' => 'Identifiant numérique d\'un auteur',
+	'label_descriptif_depot' => 'Descriptif du dépôt',
+	'label_titre_depot' => 'Titre du dépôt',
+	
+	// T
+	'titre_logo_hal' => 'Logo du dépôt',
+);
+
+?>
---
+++ lang/hals_publication_fr.php
@@ -0,0 +1,11 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'icone_modifier_publication' => 'Modifier cette publication',
+);
+
+?>
