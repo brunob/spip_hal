1ba3814b09ff1caa6cdecafe6539d26108ec1462
kent1 <kent1@arscenic.info>
2014-05-27 17:56:23 +0200

    Personne n'a le droit de créer une publication

--- hal_autoriser.php
+++ hal_autoriser.php
@@ -5,4 +5,14 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 // fonction pour le pipeline
 function hal_autoriser() {}
 
+/**
+ * Creer une nouvelle publication :
+ * 
+ * Interdit, seul le CRON peut créer une publication
+ * 
+ * @return bool false
+ */ 
+function autoriser_halspublication_creer_dist($faire, $type, $id, $qui, $opt){
+	return false;
+}
 ?>
