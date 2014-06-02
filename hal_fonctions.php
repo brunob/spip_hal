e9466f635e19dc2b394c2ba8369f41ca31a483f6
kent1 <kent1@arscenic.info>
2014-06-02 17:53:11 +0200

    Une fonction pour afficher le bon format de date

--- hal_fonctions.php
+++ hal_fonctions.php
@@ -5,4 +5,18 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 function typdoc_propre($texte){
 	return _T('hals_publication:typdoc_'.strtolower($texte));
 }
+
+function hal_affdate($date,$date_format){
+	if(in_array($date_format,array('annee','mois','jour','complet'))){
+		switch ($date_format) {
+			case 'annee':
+				$date = affdate($date,'Y');
+				break;
+			default :
+				$date = affdate_mois_annee($date);
+				break;
+		}
+	}
+	return $date;
+}
 ?>
