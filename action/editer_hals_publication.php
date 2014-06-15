474b22aed034a607b68cef8ae596502ea012d87f
kent1 <kent1@arscenic.info>
2014-06-15 12:16:43 +0200

    Fonction de mofification de publication

---
+++ action/editer_hals_publication.php
@@ -0,0 +1,48 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+/**
+ * Modifier une publication HAL
+ *
+ * $c est un contenu (par defaut on prend le contenu via _request())
+ *
+ * @param int $id_hal
+ * @param array|bool $set
+ * @return string
+ */
+function hals_publication_modifier($id_hals_publication, $set=false) {
+	include_spip('inc/modifier');
+	$c = collecter_requests(
+		// white list
+		objet_info('hals_publication','champs_editables'),
+		// black list
+		array('statut', 'date'),
+		// donnees eventuellement fournies
+		$set
+	);
+	// Si le dépot est publie, invalider les caches et demander sa reindexation
+	$t = sql_getfetsel("statut", "spip_hals_publications", "id_hals_publication=".intval($id_hals_publication));
+	if ($t == 'publie') {
+		$invalideur = "id='hals_publication/$id_hals_publication'";
+		$indexation = true;
+	}
+
+	if ($err = objet_modifier_champs('hals_publication', $id_hals_publication,
+		array(
+			'nonvide' => array(),
+			'invalideur' => $invalideur,
+			'indexation' => $indexation
+		),
+		$c))
+		return $err;
+
+	// Modification de statut, changement de rubrique ?
+	$c = collecter_requests(array('date', 'statut'),array(),$set);
+	include_spip('action/editer_objet');
+	$err = objet_instituer('hals_publication',$id_hals_publication, $c);
+
+	return $err;
+}
+
+?>
