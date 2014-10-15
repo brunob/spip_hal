38c0f7878d3c603305c7e31c82500ce61b96e58d
kent1 <kent1@arscenic.info>
2014-10-15 11:05:27 +0200

    Les autorisations complétées

--- hal_autoriser.php
+++ hal_autoriser.php
@@ -12,7 +12,30 @@ function hal_autoriser() {}
  * 
  * @return bool false
  */ 
+ 
+function autoriser_hal_creer_dist($faire, $type, $id, $qui, $opt){
+	return in_array($qui['statut'],array('0minirezo','1comite'));
+}
+
 function autoriser_halspublication_creer_dist($faire, $type, $id, $qui, $opt){
 	return false;
+}
+
+function autoriser_halspublication_modifier_dist($faire, $type, $id, $qui, $opt){
+	if($qui['statut'] == '0minirezo' || auteur_hals_publication($id,$qui) && in_array($qui['statut'],array('1comite')))
+		return true;
+	
+	return false;
+}
+
+function autoriser_hal_modifier_dist($faire, $type, $id, $qui, $opt){
+	if($qui['statut'] == '0minirezo' || auteur_hal($id,$qui) && in_array($qui['statut'],array('1comite')))
+		return true;
+	
+	return false;
+}
+
+function autoriser_hal_moderer_dist($faire, $type, $id, $qui, $opt){
+	return autoriser('modifier','hal',$id,$qui,$opt);
 }
 ?>
