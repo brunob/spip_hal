0becc9c507b1c17018486318175afdbde120688f
kent1 <kent1@arscenic.info>
2014-10-15 11:06:54 +0200

    Fonctions nécessaires aux autorisations

--- hal_fonctions.php
+++ hal_fonctions.php
@@ -19,4 +19,19 @@ function hal_affdate($date,$date_format){
 	}
 	return $date;
 }
+
+function auteur_hal($id_hal,$qui=array()){
+	if(isset($qui['statut'])){
+		return sql_allfetsel("id_auteur", "spip_auteurs_liens", "objet='hal' AND id_objet=".intval($id_hal)." AND id_auteur=".intval($qui['id_auteur']));
+	}
+	return false;
+}
+
+function auteur_hals_publication($id_hals_publication,$qui=array()){
+	if(isset($qui['statut'])){
+		$id_hal = sql_getfetsel('id_hal','spip_hals_publications','id_hals_publication='.intval($id_hals_publication));
+		return sql_allfetsel("id_auteur", "spip_auteurs_liens", "objet='hal' AND id_objet=".intval($id_hal)." AND id_auteur=".intval($qui['id_auteur']));
+	}
+	return false;
+}
 ?>
