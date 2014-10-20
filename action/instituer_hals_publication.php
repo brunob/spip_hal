de3418191a5a71ec7e2c0cc9cab3d37997d7e3da
kent1 <kent1@arscenic.info>
2014-10-20 19:50:25 +0200

    Invalider le cache lors de la dépublication

--- action/instituer_hals_publication.php
+++ action/instituer_hals_publication.php
@@ -2,7 +2,6 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-// http://doc.spip.org/@action_instituer_syndic_article_dist
 function action_instituer_hals_publication_dist() {
 
 	$securiser_action = charger_fonction('securiser_action', 'inc');
@@ -14,8 +13,9 @@ function action_instituer_hals_publication_dist() {
 		AND $id_hal = sql_getfetsel('id_hal','spip_hals_publications',"id_hals_publication=".intval($id_hals_publication))
 		AND autoriser('moderer','hal',$id_hal)) {
 		sql_updateq("spip_hals_publications", array("statut" => $statut), "id_hals_publication=".intval($id_hals_publication));
+		include_spip('inc/invalideur');
+		suivre_invalideur("id='id_hals_publication/$id_hals_publication'");
 	}
-
 }
 
 ?>
