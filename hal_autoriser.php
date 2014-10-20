88af18038b17aef0b2b4cdd920b4a1aa57cbd411
kent1 <kent1@arscenic.info>
2014-10-20 19:51:23 +0200

    Les fonctions nécessaires aux autorisations dans le bon fichier
    Il arrive des fois que le fichier de fonctions ne soit pas chargé, on
    déplace les fonctions nécessaires aux autorisations

--- hal_autoriser.php
+++ hal_autoriser.php
@@ -3,29 +3,10 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 // fonction pour le pipeline
-function hal_autoriser() {}
+function hal_autoriser() {} 
 
-/**
- * Creer une nouvelle publication :
- * 
- * Interdit, seul le CRON peut créer une publication
- * 
- * @return bool false
- */ 
- 
 function autoriser_hal_creer_dist($faire, $type, $id, $qui, $opt){
 	return in_array($qui['statut'],array('0minirezo','1comite'));
-}
-
-function autoriser_halspublication_creer_dist($faire, $type, $id, $qui, $opt){
-	return false;
-}
-
-function autoriser_halspublication_modifier_dist($faire, $type, $id, $qui, $opt){
-	if($qui['statut'] == '0minirezo' || auteur_hals_publication($id,$qui) && in_array($qui['statut'],array('1comite')))
-		return true;
-	
-	return false;
 }
 
 function autoriser_hal_modifier_dist($faire, $type, $id, $qui, $opt){
@@ -37,5 +18,38 @@ function autoriser_hal_modifier_dist($faire, $type, $id, $qui, $opt){
 
 function autoriser_hal_moderer_dist($faire, $type, $id, $qui, $opt){
 	return autoriser('modifier','hal',$id,$qui,$opt);
+}
+
+/**
+ * Creer une nouvelle publication :
+ * 
+ * Interdit, seul le CRON peut créer une publication
+ * 
+ * @return bool false
+ */
+function autoriser_halspublication_creer_dist($faire, $type, $id, $qui, $opt){
+	return false;
+}
+
+function autoriser_halspublication_modifier_dist($faire, $type, $id, $qui, $opt){
+	if($qui['statut'] == '0minirezo' || auteur_hals_publication($id,$qui) && in_array($qui['statut'],array('1comite')))
+		return true;
+	
+	return false;
+}
+
+function auteur_hal($id_hal,$qui=array()){
+	if(isset($qui['statut'])){
+		return sql_allfetsel("id_auteur", "spip_auteurs_liens", "objet='hal' AND id_objet=".intval($id_hal)." AND id_auteur=".intval($qui['id_auteur']));
+	}
+	return false;
+}
+
+function auteur_hals_publication($id_hals_publication,$qui=array()){
+	if(isset($qui['statut'])){
+		$id_hal = sql_getfetsel('id_hal','spip_hals_publications','id_hals_publication='.intval($id_hals_publication));
+		return sql_allfetsel("id_auteur", "spip_auteurs_liens", "objet='hal' AND id_objet=".intval($id_hal)." AND id_auteur=".intval($qui['id_auteur']));
+	}
+	return false;
 }
 ?>
