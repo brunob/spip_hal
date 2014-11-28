6b35753d49518a04d4e48b251fdba1b212087544
Julien Barnier <julien@nozav.org>
2014-11-28 17:22:16 +0100

    Gestion du référencement des dépôts par un idHal

--- formulaires/editer_hal.php
+++ formulaires/editer_hal.php
@@ -16,6 +16,8 @@ include_spip('inc/editer');
  */
 function formulaires_editer_hal_charger_dist($id_hal='new', $retour='', $config_fonc='hals_edit_config', $row=array(), $hidden=''){
 	$valeurs = formulaires_editer_objet_charger('hal', $id_hal, null, null, $retour, '',$config_fonc);
+        if ($valeurs['authid'] != 0) $valeurs['halid'] = $valeurs['authid'];
+        if ($valeurs['idhal'] != '') $valeurs['halid'] = $valeurs['idhal'];
 	return $valeurs;
 }
 
@@ -30,7 +32,7 @@ function formulaires_editer_hal_charger_dist($id_hal='new', $retour='', $config_
  * 
  */
 function formulaires_editer_hal_verifier_dist($id_hal='new', $retour='', $config_fonc='hals_edit_config', $row=array(), $hidden=''){
-	$erreurs = formulaires_editer_objet_verifier('hal', $id_hal,array('titre','authid'));
+	$erreurs = formulaires_editer_objet_verifier('hal', $id_hal,array('titre','halid'));
 	return $erreurs;
 }
 
@@ -46,7 +48,17 @@ function hals_edit_config($row){
  * 
  */
 function formulaires_editer_hal_traiter_dist($id_hal='new', $retour='', $config_fonc='hals_edit_config', $row=array(), $hidden=''){
-	return formulaires_editer_objet_traiter('hal', $id_hal, '', '', $retour, '',$config_fonc,$row,$hidden);
+    // numérique -> authid
+    if(is_numeric(_request('halid'))) {
+        set_request('authid', _request('halid'));
+        set_request('idhal', '');
+    }
+    // sinon -> idHal
+    else {
+        set_request('authid', 0);
+        set_request('idhal', _request('halid'));
+    }
+    return formulaires_editer_objet_traiter('hal', $id_hal, '', '', $retour, '',$config_fonc,$row,$hidden);
 }
 
 ?>
