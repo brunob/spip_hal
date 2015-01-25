d14e6da069229c568473a11524e9d57ced1de514
kent1 <kent1@arscenic.info>
2015-01-25 16:27:13 +0100

    Indentation

--- formulaires/editer_hal.php
+++ formulaires/editer_hal.php
@@ -16,8 +16,8 @@ include_spip('inc/editer');
  */
 function formulaires_editer_hal_charger_dist($id_hal='new', $retour='', $config_fonc='hals_edit_config', $row=array(), $hidden=''){
 	$valeurs = formulaires_editer_objet_charger('hal', $id_hal, null, null, $retour, '',$config_fonc);
-        if ($valeurs['authid'] != 0) $valeurs['halid'] = $valeurs['authid'];
-        if ($valeurs['idhal'] != '') $valeurs['halid'] = $valeurs['idhal'];
+	if ($valeurs['authid'] != 0) $valeurs['halid'] = $valeurs['authid'];
+	if ($valeurs['idhal'] != '') $valeurs['halid'] = $valeurs['idhal'];
 	return $valeurs;
 }
 
@@ -48,17 +48,17 @@ function hals_edit_config($row){
  * 
  */
 function formulaires_editer_hal_traiter_dist($id_hal='new', $retour='', $config_fonc='hals_edit_config', $row=array(), $hidden=''){
-    // numérique -> authid
-    if(is_numeric(_request('halid'))) {
-        set_request('authid', _request('halid'));
-        set_request('idhal', '');
-    }
-    // sinon -> idHal
-    else {
-        set_request('authid', 0);
-        set_request('idhal', _request('halid'));
-    }
-    return formulaires_editer_objet_traiter('hal', $id_hal, '', '', $retour, '',$config_fonc,$row,$hidden);
+	// numérique -> authid
+	if(is_numeric(_request('halid'))) {
+		set_request('authid', _request('halid'));
+		set_request('idhal', '');
+	}
+	// sinon -> idHal
+	else {
+		set_request('authid', 0);
+		set_request('idhal', _request('halid'));
+	}
+	return formulaires_editer_objet_traiter('hal', $id_hal, '', '', $retour, '',$config_fonc,$row,$hidden);
 }
 
 ?>
